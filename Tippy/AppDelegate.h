ea6d199c7dbc193a5380dc497f97a6cdab35f068
Britney Ting <britneyting@fb.com>
2019-06-25 14:24:40 -0700

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by britneyting on 6/25/19.
+//  Copyright © 2019 britneyting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
